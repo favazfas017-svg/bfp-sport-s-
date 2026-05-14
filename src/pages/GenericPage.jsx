ecca6a5761feeb2cbed062e45a5a0fe414cc8967
favazfas017-svg <favazfas017@gmail.com>
2026-05-14 19:48:42 +0530

    Delete src directory

--- src/pages/GenericPage.jsx
+++
@@ -1,9 +0,0 @@
-import SectionCard from '../components/SectionCard';
-
-export default function GenericPage({ title, children }) {
-  return (
-    <div className="space-y-6">
-      <SectionCard title={title}>{children}</SectionCard>
-    </div>
-  );
-}
